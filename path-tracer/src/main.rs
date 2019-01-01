03336f1b313d3d14573fbe178e7031baf3628e91
David Wicks <wicks.db@gmail.com>
2019-01-01 16:50:18 -0500

    Use a vector
    no good reason here

--- path-tracer/src/main.rs
+++ path-tracer/src/main.rs
@@ -7,21 +7,11 @@ fn main() -> std::io::Result<()> {
     let nx = 200;
     let ny = 100;
 
-    let rgb = Vec3(0.0, 0.5, 1.0) * Vec3(1.0, 2.0, 2.0);
-    println!(
-        "Have a vector with rgb values: {}, {}, {}",
-        rgb.r(),
-        rgb.g(),
-        rgb.b()
-    );
-
     let buffer = ImageBuffer::from_fn(nx, ny, |x, y| {
-        let r = x as f32 / nx as f32;
-        let g = (ny - y) as f32 / ny as f32;
-        let b = 0.2;
-        let ir: u8 = (255.99 * r) as u8;
-        let ig: u8 = (255.99 * g) as u8;
-        let ib: u8 = (255.99 * b) as u8;
+        let rgb = Vec3(x as f32 / nx as f32, (ny - y) as f32 / ny as f32, 0.2);
+        let ir: u8 = (255.99 * rgb.r()) as u8;
+        let ig: u8 = (255.99 * rgb.g()) as u8;
+        let ib: u8 = (255.99 * rgb.b()) as u8;
 
         image::Rgb([ir, ig, ib])
     });
