036fbec288a73e742c6c8a190bd92a9d5e415f31
David Wicks <wicks.db@gmail.com>
2019-01-01 12:54:48 -0500

    output PNG file
    Render a gradient to PNG

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+extern crate image;
+
+use image::ImageBuffer;
+
+fn main() -> std::io::Result<()> {
+    let nx = 200;
+    let ny = 100;
+
+    let buffer = ImageBuffer::from_fn(nx, ny, |x, y| {
+        let r = x as f32 / nx as f32;
+        let g = (ny - y) as f32 / ny as f32;
+        let b = 0.2;
+        let ir: u8 = (255.99 * r) as u8;
+        let ig: u8 = (255.99 * g) as u8;
+        let ib: u8 = (255.99 * b) as u8;
+
+        image::Rgb([ir, ig, ib])
+    });
+
+    buffer.save("image.png")?;
+
+    Ok(())
 }
