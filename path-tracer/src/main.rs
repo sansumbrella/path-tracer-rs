18f84cfe4926d6a120890818cf53a3b2e4372966
David Wicks <wicks.db@gmail.com>
2019-01-02 20:41:47 -0500

    Hitable collection

--- path-tracer/src/main.rs
+++ path-tracer/src/main.rs
@@ -15,6 +15,17 @@ fn main() -> std::io::Result<()> {
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
 
+    let mut world = World(vec![]);
+    world.collection().push(Box::new(Sphere {
+        center: Vec3::new(0.2, 0.0, -1.0),
+        radius: 0.6,
+    }));
+
+    world.collection().push(Box::new(Sphere {
+        center: Vec3::new(-0.3, 0.1, -0.8),
+        radius: 0.3,
+    }));
+
     let buffer = ImageBuffer::from_fn(nx, ny, |x, y| {
         let u = x as f32 / nx as f32;
         let v = (ny - y) as f32 / ny as f32;
@@ -23,7 +34,7 @@ fn main() -> std::io::Result<()> {
             origin,
             lower_left_corner + (horizontal * u) + (vertical * v),
         );
-        let rgb = color(ray);
+        let rgb = color(&world, ray);
         let ir: u8 = (255.99 * rgb.r()) as u8;
         let ig: u8 = (255.99 * rgb.g()) as u8;
         let ib: u8 = (255.99 * rgb.b()) as u8;
@@ -36,16 +47,34 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn color(ray: Ray) -> Vec3 {
-    let sphere = Sphere {
-        center: Vec3::new(-0.2, 0.0, -1.0),
-        radius: 0.6,
-    };
-    if let Some(hit) = sphere.hit(&ray, 0.0, 1.0) {
+fn color(world: &World, ray: Ray) -> Vec3 {
+    if let Some(hit) = world.hit(&ray, 0.0, 1.0) {
         return (hit.normal + 1.0) * 0.5;
     }
 
     let unit_direction = normalize(ray.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     mix(Vec3::new(1.0, 1.0, 1.0), Vec3::new(0.5, 0.7, 1.0), t)
+}
+
+struct World(Vec<Box<Hitable>>);
+
+impl Hitable for World {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut closest = t_max;
+        let mut found: Option<HitRecord> = None;
+        for hitable in &self.0 {
+            if let Some(hit) = hitable.hit(ray, t_min, closest) {
+                closest = hit.t;
+                found = Some(hit);
+            }
+        }
+        found
+    }
+}
+
+impl World {
+    pub fn collection(&mut self) -> &mut Vec<Box<Hitable>> {
+        &mut self.0
+    }
 }
